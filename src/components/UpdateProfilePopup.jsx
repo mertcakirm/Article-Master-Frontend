f59db03effbc7ec1eb73df139632c78229ab5301
mertcakir <mertcakirm3377@gmail.com>
2025-03-18 22:49:17 +0300

    .

---
+++ src/components/UpdateProfilePopup.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './css/accepted.css';
+
+const UpdateProfilePopup = ({onClose}) => {
+    return (
+        <div className="popup-overlay">
+            <div className="popup-content"  style={{color:'#fff'}} data-aos="zoom-in" >
+                <button className="popup-close-btn" onClick={() => onClose(false)}>&times;</button>
+                <div className="row justify-content-center align-items-center row-gap-3">
+
+                        <input className="col-12 profile_inp" placeholder="New Username" type="text" />
+
+                        <input className="col-12 profile_inp" placeholder="Last Password" type="password" />
+
+                        <input className="col-12 profile_inp"  placeholder="New Password" type="password" />
+
+                        <button  onClick={() => onClose(false)} className="profile_btn">Update Information</button>
+
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default UpdateProfilePopup;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from "../components/Navbar.jsx";
 import './css/profile.css'
+import UpdateProfilePopup from "../components/UpdateProfilePopup.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Profile = () => {
     const [updatePopup, setUpdatePopup] = useState(false);
@@ -9,6 +12,10 @@ const Profile = () => {
         setUpdatePopup(!updatePopup);
     };
 
+    useEffect(() => {
+        AOS.init({ duration: 500 });
+    }, []);
+
     return (
         <div>
             <Navbar />
@@ -16,10 +23,11 @@ const Profile = () => {
                 <div className="row justify-content-center py-5">
 
                     <div className="col-lg-6 row justify-content-center align-items-center">
-                        <h3 className="text-center col-12">Profile Card</h3>
-                        <div className="col-8 mt-3">
-                            <div className="profile_card row justify-content-between p-3">
-                                    <div className="col-4 row justify-content-center align-items-center row-gap-2">
+                        <h3 className="text-center col-12" data-aos="fade-in">Profile Card</h3>
+                        <div className="col-lg-8 col-12 mt-3">
+                            <div className="profile_card row justify-content-between p-3"  data-aos="fade-up">
+
+                                    <div className="col-lg-4 col-12 row justify-content-center align-items-center row-gap-2">
                                         <div className="p-0" style={{
                                             position: 'relative',
                                             overflow: 'hidden',
@@ -30,11 +38,12 @@ const Profile = () => {
                                                  src="https://image.hurimg.com/i/hurriyet/75/750x422/596730b4c03c0e32bc3e59a2.jpg"
                                                  alt="profile_photo"/>
                                         </div>
-                                        <div className="profile-card-left-info col-8 text-center">Mert Çakır</div>
-                                        <div className="profile-card-left-info col-8 border-0 text-center">Writer</div>
+
+                                        <div className="profile-card-left-info col-lg-8 col-12 text-center">Mert Çakır</div>
+                                        <div className="profile-card-left-info col-lg-8 col-12 border-0 text-center">Writer</div>
                                     </div>
 
-                                    <div className="col-8 justify-content-center align-items-start row-gap-3" style={{display:'flex',flexDirection:'column'}}>
+                                    <div className="col-lg-8 col-12 justify-content-center align-items-start row-gap-3" style={{display:'flex',flexDirection:'column'}}>
                                         <div className="w-100 row justify-content-between align-items-center p-0 m-0">
                                             <div className="col-9 profile-card-right-key">Article Count</div>
                                             <div className="col-3 profile-card-right-value">12</div>
@@ -55,16 +64,20 @@ const Profile = () => {
                                             <div className="col-9 profile-card-right-key">Comments Count</div>
                                             <div className="col-3 profile-card-right-value">75</div>
                                         </div>
-                                        <button className="my-notes-process-see col-12">Update Other Information</button>
+                                        <button onClick={toggleUpdatePopup} className="my-notes-process-see col-12">Update Other Information</button>
                                     </div>
                             </div>
                         </div>
-
-
-                        </div>
                     </div>
                 </div>
-
+            </div>
+            {updatePopup && (
+                <UpdateProfilePopup
+                    onClose={(b) => {
+                        if (b === false) setUpdatePopup(b);
+                    }}
+                />
+            )}
         </div>
     );
 };
