018dd4bf5c8876167a5cda956f313e8c61258aa0
mertcakir <mertcakirm3377@gmail.com>
2025-03-28 00:05:27 +0300

    .

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -24,6 +24,7 @@ const Articles = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
                     </div>
 
                     <div className="col-lg-3" data-aos="fade-up">
@@ -34,6 +35,7 @@ const Articles = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
                     </div>
 
                     <div className="col-lg-3" data-aos="fade-up">
@@ -44,8 +46,8 @@ const Articles = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
                     </div>
-
                     <div className="col-lg-3" data-aos="fade-up">
                         <a href="/articles/1" className="article-card">
                             <img className="w-100 img-fluid article-card-img" alt="article-image"
@@ -54,8 +56,8 @@ const Articles = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
                     </div>
-
                     <div className="col-lg-3" data-aos="fade-up">
                         <a href="/articles/1" className="article-card">
                             <img className="w-100 img-fluid article-card-img" alt="article-image"
@@ -64,8 +66,8 @@ const Articles = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
                     </div>
-
                     <div className="col-lg-3" data-aos="fade-up">
                         <a href="/articles/1" className="article-card">
                             <img className="w-100 img-fluid article-card-img" alt="article-image"
@@ -74,27 +76,9 @@ const Articles = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
                     </div>
 
-                    <div className="col-lg-3" data-aos="fade-up">
-                        <a href="/articles/1" className="article-card">
-                            <img className="w-100 img-fluid article-card-img" alt="article-image"
-                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
-                            <p className="article-card-title">Article Title</p>
-                            <p className="article-card-desc">Article Description Article Description Article Description
-                                Article Description Article Description Article Description Article Description</p>
-                        </a>
-                    </div>
-
-                    <div className="col-lg-3" data-aos="fade-up">
-                        <a href="/articles/1" className="article-card">
-                            <img className="w-100 img-fluid article-card-img" alt="article-image"
-                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
-                            <p className="article-card-title">Article Title</p>
-                            <p className="article-card-desc">Article Description Article Description Article Description
-                                Article Description Article Description Article Description Article Description</p>
-                        </a>
-                    </div>
                 </div>
                 <div  className="my-notes-process-flex mt-5">
                     {pageNum > 1 && (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,8 +34,8 @@ const Home = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
                     </div>
-
                     <div className="col-lg-3" data-aos="fade-up">
                         <a href="/articles/1" className="article-card">
                             <img className="w-100 img-fluid article-card-img" alt="article-image"
@@ -44,8 +44,8 @@ const Home = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
                     </div>
-
                     <div className="col-lg-3" data-aos="fade-up">
                         <a href="/articles/1" className="article-card">
                             <img className="w-100 img-fluid article-card-img" alt="article-image"
@@ -54,17 +54,18 @@ const Home = () => {
                             <p className="article-card-desc">Article Description Article Description Article Description
                                 Article Description Article Description Article Description Article Description</p>
                         </a>
-                    </div>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
+                    </div>                    <div className="col-lg-3" data-aos="fade-up">
+                    <a href="/articles/1" className="article-card">
+                        <img className="w-100 img-fluid article-card-img" alt="article-image"
+                             src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
+                        <p className="article-card-title">Article Title</p>
+                        <p className="article-card-desc">Article Description Article Description Article Description
+                            Article Description Article Description Article Description Article Description</p>
+                    </a>
+                    <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
+                </div>
 
-                    <div className="col-lg-3" data-aos="fade-up">
-                        <a href="/articles/1" className="article-card">
-                            <img className="w-100 img-fluid article-card-img" alt="article-image"
-                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
-                            <p className="article-card-title">Article Title</p>
-                            <p className="article-card-desc">Article Description Article Description Article Description
-                                Article Description Article Description Article Description Article Description</p>
-                        </a>
-                    </div>
 
                     <div className="col-12 row justify-content-center">
                         <a href="/articles" className="animation-btn">
