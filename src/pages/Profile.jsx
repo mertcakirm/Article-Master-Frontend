ad4c15a533cb05137be3636afe69ac734244ef6b
mertcakir <mertcakirm3377@gmail.com>
2025-03-28 00:10:08 +0300

    .

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -47,7 +47,7 @@ const Profile = () => {
                             <h3 className="text-center col-12" data-aos="fade-in">Profile Card</h3>
 
                             <div className="profile_card row justify-content-between p-3 mt-5"  data-aos="fade-up">
-                                    <div className="col-lg-4 col-12 row justify-content-center align-items-center row-gap-2">
+                                    <div className="col-lg-5 col-12 row justify-content-center align-items-center row-gap-2">
                                         <div className="p-0" style={{
                                             position: 'relative',
                                             overflow: 'hidden',
@@ -63,7 +63,7 @@ const Profile = () => {
                                         <div className="profile-card-left-info col-lg-8 col-12 border-0 text-center">Writer</div>
                                     </div>
 
-                                    <div className="col-lg-8 col-12 justify-content-center align-items-start row-gap-3" style={{display:'flex',flexDirection:'column'}}>
+                                    <div className="col-lg-7 col-12 justify-content-center align-items-start row-gap-3" style={{display:'flex',flexDirection:'column'}}>
                                         <div className="w-100 row justify-content-between align-items-center p-0 m-0">
                                             <div className="col-9 profile-card-right-key">Article Count</div>
                                             <div className="col-3 profile-card-right-value">12</div>
