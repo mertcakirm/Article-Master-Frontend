93c1386b468617075de19980f526436fc41b963f
mertcakir <mertcakirm3377@gmail.com>
2025-03-30 19:13:10 +0300

    .

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,15 +1,114 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from "../components/Navbar.jsx";
+import ProcessPopup from "../components/popups/processPopup.jsx";
 
 const Admin = () => {
+    const [isProcessPopupOpen, setProcessIsPopupOpen] = useState(false);
+
+    const toggleProcessPopup = () => {
+        setProcessIsPopupOpen(!isProcessPopupOpen);
+    };
     return (
         <div>
             <Navbar />
             <div className="page-container mb-5 container-fluid">
-                <div className="row row-gap-5 popular-row">
+                <div className="row row-gap-5 popular-row ">
+                    <div className="col-lg-6 mt-5 row justify-content-center">
+                        <div className="titles text-center">Role Requests</div>
+                        <table className="table table-striped table-dark text-center" style={{borderRadius: '10px',overflow: 'hidden'}} >
+                            <thead>
+                            <tr>
+                                <th scope="col">Writer Name</th>
+                                <th scope="col">File</th>
+                                <th scope="col">Process</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">Article 1</th>
+                                <td>Alan Abel</td>
+                                <td>
+                                    <div className="my-notes-process-flex">
+                                        <a href="/articles/1" className="my-notes-process-see">
+                                            <svg clipRule="evenodd" fill="white" width="30" height="30" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                        </a>
+                                        <button onClick={toggleProcessPopup} className="my-notes-process-bin">
+                                            <svg  fill="white" width="24" height="24"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/></svg>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Article 1</th>
+                                <td>Alan Abel</td>
+                                <td>
+                                    <div className="my-notes-process-flex">
+                                        <a href="/articles/1" className="my-notes-process-see">
+                                            <svg clipRule="evenodd" fill="white" width="30" height="30" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                        </a>
+                                        <button onClick={toggleProcessPopup} className="my-notes-process-bin">
+                                            <svg  fill="white" width="24" height="24"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/></svg>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Article 1</th>
+                                <td>Alan Abel</td>
+                                <td>
+                                    <div className="my-notes-process-flex">
+                                        <a href="/articles/1" className="my-notes-process-see">
+                                            <svg clipRule="evenodd" fill="white" width="30" height="30" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                        </a>
+                                        <button onClick={toggleProcessPopup} className="my-notes-process-bin">
+                                            <svg  fill="white" width="24" height="24"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/></svg>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+
+                        <div  className="my-notes-process-flex">
+                            {pageNum > 1 && (
+                                <button onClick={() => setPageNum(pageNum - 1)} className="my-notes-process-see">
+                                    <svg
+                                        fill="white"
+                                        width="34"
+                                        height="36"
+                                        clipRule="evenodd"
+                                        fillRule="evenodd"
+                                        strokeLinejoin="round"
+                                        strokeMiterlimit="2"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591 1.299-1.002 3.945-3.044 5.498-4.243z"/>
+                                    </svg>
+                                </button>
+                            )}
+
+                            <div className="my-notes-process-see text-center" style={{width:'40px',lineHeight:'40px'}}>{pageNum}</div>
+                            <button onClick={()=>setPageNum(pageNum+1)} className="my-notes-process-see">
+                                <svg  fill="white" width="34" height="36" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z"/></svg>
+                            </button>
+                        </div>
+                    </div>
 
                 </div>
             </div>
+            {isProcessPopupOpen && (
+                <ProcessPopup
+                    onClose={(b) => {
+                        if (b === false) setProcessIsPopupOpen(b);
+                    }}
+                    text="Bu makaleye ait notlarınızı silmek istediğinize emin misiniz?"
+                    acceptedText="Notlar başarıyla silindi"
+                    type="offer_delete"
+                    id="1"
+                />
+            )}
         </div>
     );
 };
--- src/pages/WriterArticles.jsx
+++ src/pages/WriterArticles.jsx
@@ -1,15 +1,176 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from "../components/Navbar.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const WriterArticles = () => {
+    const [pageNum, setPageNum] = useState(1);
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    }, []);
+
+
     return (
         <div>
             <Navbar />
             <div className="page-container mb-5 container-fluid">
                 <div className="row row-gap-5 popular-row">
+                    <div className="col-12 titles mt-5 mb-4 text-center" data-aos="fade-in">Alan Abel Articles</div>
+
+                    <div className="col-lg-3" data-aos="fade-up">
+                        <a href="/articles/1" className="article-card">
+                            <img className="w-100 img-fluid article-card-img" alt="article-image"
+                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
+                            <p className="article-card-title">Article Title</p>
+                            <p className="article-card-desc">Article Description Article Description Article Description
+                                Article Description Article Description Article Description Article Description</p>
+                            <div className="my-notes-process-flex justify-content-between w-100 px-5">
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" fill="white" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                    <div>123</div>
+                                </div>
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="yellow" height="25" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                    <div>9/10</div>
+                                </div>
+                            </div>
+                        </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
+                    </div>
+
+                    <div className="col-lg-3" data-aos="fade-up">
+                        <a href="/articles/1" className="article-card">
+                            <img className="w-100 img-fluid article-card-img" alt="article-image"
+                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
+                            <p className="article-card-title">Article Title</p>
+                            <p className="article-card-desc">Article Description Article Description Article Description
+                                Article Description Article Description Article Description Article Description</p>
+                            <div className="my-notes-process-flex justify-content-between w-100 px-5">
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" fill="white" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                    <div>123</div>
+                                </div>
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="yellow" height="25" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                    <div>9/10</div>
+                                </div>
+                            </div>
+                        </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
+                    </div>
+
+                    <div className="col-lg-3" data-aos="fade-up">
+                        <a href="/articles/1" className="article-card">
+                            <img className="w-100 img-fluid article-card-img" alt="article-image"
+                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
+                            <p className="article-card-title">Article Title</p>
+                            <p className="article-card-desc">Article Description Article Description Article Description
+                                Article Description Article Description Article Description Article Description</p>
+                            <div className="my-notes-process-flex justify-content-between w-100 px-5">
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" fill="white" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                    <div>123</div>
+                                </div>
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="yellow" height="25" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                    <div>9/10</div>
+                                </div>
+                            </div>
+                        </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
+                    </div>
+
+                    <div className="col-lg-3" data-aos="fade-up">
+                        <a href="/articles/1" className="article-card">
+                            <img className="w-100 img-fluid article-card-img" alt="article-image"
+                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
+                            <p className="article-card-title">Article Title</p>
+                            <p className="article-card-desc">Article Description Article Description Article Description
+                                Article Description Article Description Article Description Article Description</p>
+                            <div className="my-notes-process-flex justify-content-between w-100 px-5">
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" fill="white" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                    <div>123</div>
+                                </div>
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="yellow" height="25" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                    <div>9/10</div>
+                                </div>
+                            </div>
+                        </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
+                    </div>
+
+                    <div className="col-lg-3" data-aos="fade-up">
+                        <a href="/articles/1" className="article-card">
+                            <img className="w-100 img-fluid article-card-img" alt="article-image"
+                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
+                            <p className="article-card-title">Article Title</p>
+                            <p className="article-card-desc">Article Description Article Description Article Description
+                                Article Description Article Description Article Description Article Description</p>
+                            <div className="my-notes-process-flex justify-content-between w-100 px-5">
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" fill="white" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                    <div>123</div>
+                                </div>
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="yellow" height="25" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                    <div>9/10</div>
+                                </div>
+                            </div>
+                        </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
+                    </div>
+
+                    <div className="col-lg-3" data-aos="fade-up">
+                        <a href="/articles/1" className="article-card">
+                            <img className="w-100 img-fluid article-card-img" alt="article-image"
+                                 src="https://bgcp.bionluk.com/images/portfolio/1400x788/d9792dfe-2379-4a8f-bc5f-3c1649310174.jpg"/>
+                            <p className="article-card-title">Article Title</p>
+                            <p className="article-card-desc">Article Description Article Description Article Description
+                                Article Description Article Description Article Description Article Description</p>
+                            <div className="my-notes-process-flex justify-content-between w-100 px-5">
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" fill="white" height="30" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 5c-4.078 0-7.742 3.093-9.853 6.483-.096.159-.145.338-.145.517s.048.358.144.517c2.112 3.39 5.776 6.483 9.854 6.483 4.143 0 7.796-3.09 9.864-6.493.092-.156.138-.332.138-.507s-.046-.351-.138-.507c-2.068-3.403-5.721-6.493-9.864-6.493zm.002 3c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4zm0 1.5c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5z" fillRule="nonzero"/></svg>
+                                    <div>123</div>
+                                </div>
+                                <div className="my-notes-process-flex align-items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="yellow" height="25" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                    <div>9/10</div>
+                                </div>
+                            </div>
+                        </a>
+                        <button className="article-card-like"><svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" width="30" height="30" fill="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 5.72c-2.624-4.517-10-3.198-10 2.461 0 3.725 4.345 7.727 9.303 12.54.194.189.446.283.697.283s.503-.094.697-.283c4.977-4.831 9.303-8.814 9.303-12.54 0-5.678-7.396-6.944-10-2.461z" fillRule="nonzero"/></svg></button>
+                    </div>
 
                 </div>
+                <div  className="my-notes-process-flex mt-5">
+                    {pageNum > 1 && (
+                        <button onClick={() => setPageNum(pageNum - 1)} className="my-notes-process-see">
+                            <svg
+                                fill="white"
+                                width="34"
+                                height="36"
+                                clipRule="evenodd"
+                                fillRule="evenodd"
+                                strokeLinejoin="round"
+                                strokeMiterlimit="2"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591 1.299-1.002 3.945-3.044 5.498-4.243z"/>
+                            </svg>
+                        </button>
+                    )}
+
+                    <div className="my-notes-process-see text-center" style={{width:'40px',lineHeight:'40px'}}>{pageNum}</div>
+                    <button onClick={()=>setPageNum(pageNum+1)} className="my-notes-process-see">
+                        <svg  fill="white" width="34" height="36" clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m10.211 7.155c-.141-.108-.3-.157-.456-.157-.389 0-.755.306-.755.749v8.501c0 .445.367.75.755.75.157 0 .316-.05.457-.159 1.554-1.203 4.199-3.252 5.498-4.258.184-.142.29-.36.29-.592 0-.23-.107-.449-.291-.591-1.299-1.002-3.945-3.044-5.498-4.243z"/></svg>
+                    </button>
+                </div>
+
             </div>
+
         </div>
     );
 };
