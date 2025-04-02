e8e8591a113aeeacc3f1ac7575a4532710bf3c8f
mertcakir <mertcakirm3377@gmail.com>
2025-04-02 17:59:04 +0300

    .

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -56,7 +56,7 @@ const Articles = () => {
                                             <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="yellow" height="25" viewBox="0 0 24 24">
                                                 <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/>
                                             </svg>
-                                            <div>{article.ratingOverall} /10</div>
+                                            <div>{Number.isInteger(article.ratingOverall) ? article.ratingOverall : article.ratingOverall.toFixed(1)}/10</div>
                                         </div>
                                     </div>
                                 </a>
