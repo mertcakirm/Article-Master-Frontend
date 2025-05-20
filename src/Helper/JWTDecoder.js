5e0c4b1c78efaf2c06796f383de7714d4cc033d3
mertcakir <mertcakirm3377@gmail.com>
2025-05-20 22:03:21 +0300

    .

--- src/Helper/JWTDecoder.js
+++ src/Helper/JWTDecoder.js
@@ -1,13 +1,13 @@
 import {getCookie} from "../API/Cokkie.js";
 
-export const parseJwt=()=> {
+export const parseJwt = () => {
     const token = getCookie("token")
 
     if (!token) return null;
     try {
         const base64Url = token.split('.')[1];
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
 
